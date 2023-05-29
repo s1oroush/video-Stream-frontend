2f190b07f1ed4c25a6e1491e96bf14143d3924ad
soroush <soroush.rezaeian@gamil.com>
2023-05-29 21:06:53 +0330

    video streamer frontend first time initial !!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        clients: []
+    };
+
+
+    async componentDidMount() {
+        const videoDiv = document.querySelector('#video-player');
+        const videoScreen = document.querySelector('#video-screen');
+        const name = window.location.pathname;
+        videoScreen.src = `http://localhost:8083/video` + name;
+        videoDiv.style.display = 'block';
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div id="video-player">
+                        <video id="video-screen" width="720px" height="480px" controls></video>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
